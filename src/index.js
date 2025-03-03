6ebda441226aab048ceff235323fe7639aa07c2c
Ankit_kumar <ankit95001kumar@gmail.com>
2025-03-03 20:56:42 +0530

    [Ankit] Contact Created

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const Contact = require('./models/Contact');
+
+
+const contact1 = new Contact(
+    "Ankit", "Kumar", "Rosera", "Bihar", "India", "848210", "7870793511", "ankit95001@gmail.com"
+);
+
+console.log(contact1.displayContact())
